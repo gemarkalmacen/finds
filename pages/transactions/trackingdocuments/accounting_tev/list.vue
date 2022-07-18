f17b9eaed9b2a4333cf96c505af01656a6b36c96
Reymark <reymarkvaldehueza@gmail.com>
2022-07-18 16:33:26 +0800

    feature/c-101:Create Payroll and generate transaction number in TEV

---
+++ pages/transactions/trackingdocuments/accounting_tev/list.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <BreadCrumbs main="Tracking Documents" title="Purchase Request" />
+    <h1>This is Purchase request Page</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
